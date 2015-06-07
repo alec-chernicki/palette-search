9b22d41d81bf1ff02b8fcda4850efc98a9e33065
Alec Ortega <alecortega@live.com>
2015-06-07 02:32:35 -0400

    Adds ability to search for palettes based on keywords
    - Added regex to check valid input
    - If valid a params object is made to be sent as a query string to the API
    - Created get request to API with error handler
    - Created function to formated returned JSON
    - Created send back to Slack

---
+++ README.md
@@ -0,0 +1,18 @@
+# Palettable
+
+Color palette inspiration from directly within Slack
+
+![alt tag](http://i62.tinypic.com/2rnyl34.gif)
+
+<h2>How To Use:</h2>
+Go to any Slack channel and type /palette [search terms]. The questions will be shown on the same channel tht the command was entered and only visible to you.
+ 
+<h2> How To Install On Slack:</h2>
+1. Go to your team's Slack channel
+2. Click on your teams name, then on <b>Configure Integrations</b>
+3. Scroll to the very bottom of the page, then click on <b>DIY Integrations & Customizations section</b>
+4. Click on <b>Add</b> next to <b>Slash Commands</b>
+  - URL: ```https://alecortega-palettable.herokuapp.com/palette```
+  - Command: ```/palette```
+  - Method: ```POST```
+  - Usage Hint ```[search term]```
--- app.js
+++ app.js
@@ -1,2 +1,79 @@
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var colourlovers = require('colourlovers');
 
+// Initialize Express
+var app = express();
+
+// Set port assignment
+var port = process.env.PORT || 3000;
+
+// Parse that body middleware, four for you, you go middleware.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Change this to change how many palettes are returned
+var NUMBER_OF_PALETTES = 3;
+
+var formatColorRow = function (q) {
+
+  // Add pound to beginning of each color
+  var colors = q.colors.map(function (color) {
+    return ' #' + color;
+  });
+
+  return '*' + q.title + '*: ' + colors.toString();
+};
+
+// Main route
+app.post('/palette', function (req, res) {
+
+  var searchTerm = req.body.text;
+
+  // Check if input is a search term
+  if (/^[a-zA-Z0-9 ]+$/.test(searchTerm)) {
+
+    // Create object to send to API
+    var params = {
+      numResults: NUMBER_OF_PALETTES,
+      keywords: searchTerm,
+      keywordExact: 1
+    };
+
+    // Request palettes from COLOURlovers
+    colourlovers.get('/palettes', params, function (err, data) {
+      if (err) {
+        res.status(404).send('Uh oh, looks like something went wrong.');
+      }
+
+      // Construct list of palettes
+      var qs = ['Top color palettes for "' + searchTerm + '"\n']
+          .concat(data.map(formatColorRow));
+
+      // If no results found tell user to make broader search
+      if (qs.length === 1) {
+        res.status(204).send('No questions found. Please try a broader search');
+      }
+
+      // Send message to Slack
+      res.setHeader('content-type', 'text/plain; charset=utf-8');
+      res.status(200).send(qs.join('\r\n'));
+
+    });
+  }
+
+  // For when you can't paint with all the colors of the wind
+  else {
+    res.status(404).send(searchTerm + ' is not a valid search term, please try again.');
+  }
+});
+
+// Error Handler
+app.use(function (err, req, res) {
+	console.error(err.stack);
+	res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+	console.log('Slack bot listening on port ' + port);
+});
